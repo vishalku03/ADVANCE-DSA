6efbd5a060add391fb5dcc677d406407ecb81699
Vishal Kumar <157531413+vishalku03@users.noreply.github.com>
2024-02-12 22:42:49 +0530

    Add files via upload

---
+++ MEJORETY ELEMENT.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution
+{
+public:
+     int majorityElement(vector<int> &nums)
+     {
+          int count = 0;
+          int element = 0;
+
+          for (int i = 0; i < nums.size(); i++)
+          {
+               if (count == 0)
+               {
+                    element = nums[i];
+                    count = 1;
+               }
+
+               else if (element == nums[i])
+               {
+                    count++;
+               }
+               else
+               {
+                    count--;
+               }
+          }
+          return element;
+     }
+};
+
+// moore's voting algorithm
+// LEETCODE PROBLEM -  169
